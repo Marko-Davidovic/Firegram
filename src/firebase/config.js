edb9ab304d95b86244bd61f712776f6e784e8c80
Marko Davidovic <davidovic-m@hotmail.com>
2020-09-05 11:03:37 -0400

    prefinal commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyA7aEaYe3ucWe2BYpzNrzK27HVQuaGzxhA",
-    authDomain: "firegram-996d7.firebaseapp.com",
-    databaseURL: "https://firegram-996d7.firebaseio.com",
-    projectId: "firegram-996d7",
-    storageBucket: "firegram-996d7.appspot.com",
-    messagingSenderId: "256454383580",
-    appId: "1:256454383580:web:2691891032725bb3122119"
+    apiKey: "AIzaSyA6s2PdnDBfzAWm_1ePh0WmRbA3EvwhM4g",
+    authDomain: "firegram-fbf2f.firebaseapp.com",
+    databaseURL: "https://firegram-fbf2f.firebaseio.com",
+    projectId: "firegram-fbf2f",
+    storageBucket: "firegram-fbf2f.appspot.com",
+    messagingSenderId: "508717087670",
+    appId: "1:508717087670:web:bef6ad7b4e9ecf7254df17"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { projectFirestore } from '../firebase/confing';
+import { projectFirestore } from '../firebase/config';
 
 const useFirestore = (collection) => {
   const [docs, setDocs] = useState([]);
